f52e5261f1909705d412ec98cab0d45d68a66485
alerrandro borges <alerrandrokaton@gmail.com>
2022-04-12 17:30:29 -0300

    commit do projeto completo

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->words(3, true),
+            'description' => $this->faker->sentence,
+            'price' => $this->faker->randomFloat(2, 10, 300)
+        ];
+    }
+}
